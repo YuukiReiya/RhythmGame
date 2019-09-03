60885707bc129477abce0830a3aa6b21a34eaa08
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-03 11:56:00 +0900

    update to any file

--- SoundWaver/Assets/Scripts/Hoge.cs
+++ SoundWaver/Assets/Scripts/Hoge.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Hoge : MonoBehaviour
 {
-    AudioListener audioListener;
+    public AudioSpectrum audioSpectrum;
+    public float scale = 10;
+    public GameObject[] objects;
     // Start is called before the first frame update
     void Start ()
     {
-        audioListener = GetComponent<AudioListener> ();
+
     }
 
     // Update is called once per frame
     void Update ()
     {
+        for (int i = 0; i < objects.Length; ++i)
+        {
+            var obj = objects[i];
+            var lScale = obj.transform.localScale;
+            lScale.y = audioSpectrum.Levels[i] * scale;
+            obj.transform.localScale = lScale;
+        }
     }
 }
