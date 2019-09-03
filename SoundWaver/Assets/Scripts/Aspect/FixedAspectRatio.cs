fa0b43c0050ea5480027c28531a95749a0a6ba6a
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-03 15:38:18 +0900

    fixed aspect ratio add

---
+++ SoundWaver/Assets/Scripts/Aspect/FixedAspectRatio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FixedAspectRatio
+{
+    static float _width;
+    static float _height;
+    public static float Aspect { get { return _height / _width; } }
+
+    public static void Execute (Camera camera, float width, float height)
+    {
+        _width = width;
+        _height = height;
+        float ratio = Aspect / (Screen.height * 1.0f / Screen.width);
+        float offset = (1.0f - ratio) / 2f;
+        camera.rect = new Rect (offset, 0f, ratio, 1f);
+    }
+}
