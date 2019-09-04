240c291292dddd18d34ba2b2f3f8d42ab807b8d8
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-04 12:21:17 +0900

    fps merter add

---
+++ SoundWaver/Assets/Tool/FPSmeter/Scripts/FPSmeter.cs
@@ -0,0 +1,41 @@
+﻿/// <summary>
+/// 番場宥輝
+/// </summary>
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// FPS計測
+/// </summary>
+public class FPSmeter : MonoBehaviour {
+
+    [SerializeField,Tooltip("TextComponent")] Text text;
+
+    private int frameCount;
+    private float prevTime;
+
+	// Use this for initialization
+	void Start () {
+        frameCount = 0;
+        prevTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ++frameCount;
+        float time = Time.realtimeSinceStartup - prevTime;
+
+        if (time >= 0.5f)
+        {
+            int fps = (int)(frameCount / time);
+            //Debug.LogFormat("{0}fps", frameCount / time);
+            //Debug.LogFormat("{0}fps", fps);
+            text.text = "FPS:" + fps;
+            frameCount = 0;
+            prevTime = Time.realtimeSinceStartup;
+
+        }
+    }
+}
