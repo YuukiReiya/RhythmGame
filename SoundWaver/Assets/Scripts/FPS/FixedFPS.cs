8bf4641867bd7b6d0d8743acbd7371379195d395
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-04 12:22:23 +0900

    scripts file add

---
+++ SoundWaver/Assets/Scripts/FPS/FixedFPS.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedFPS : MonoBehaviour
+{
+    [SerializeField, Range (0, 120)]
+    int fps = 60;
+    void Awake ()
+    {
+        /// <summary>
+        /// 事前に"ProjectSetting/Quality/Other/VSync Count"をDon't Syncに設定
+        /// </summary>
+        Application.targetFrameRate = fps;
+    }
+}
