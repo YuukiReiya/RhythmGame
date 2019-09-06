6f951b3d8def436eb573033ad8792e1b25ddde72
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-06 10:40:06 +0900

    note script add.
    ノーツを曲のビートに応じてスケールさせるデコレートスクリプト追加。

---
+++ SoundWaver/Assets/Scripts/Notes/NoteDecorate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class NoteDecorate : MonoBehaviour
+{
+    [SerializeField] Vector3 from;
+    [SerializeField] Vector3 to;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Music.IsJustChangedBeat())
+        {
+            DOTween.To(t => OnScale(t), 0, 1, 0.1f);
+        }
+    }
+
+    public void OnScale(float t)
+    {
+        var scale = transform.localScale;
+        scale = Vector3.Lerp(from, to, t);
+        transform.localScale = scale;
+    }
+}
