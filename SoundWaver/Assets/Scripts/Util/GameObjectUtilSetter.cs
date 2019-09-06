2775553bb33a93d6d577f9be194e8a6ab5af93f2
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-06 10:38:55 +0900

    utility scripts add.
    他オブジェクトを参照しオブジェクトのワールド/ローカルのトランスフォームを設定するユーティリティスクリプト追加。

---
+++ SoundWaver/Assets/Scripts/Util/GameObjectUtilSetter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectUtilSetter : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField] GameObject target;
+    [SerializeField] GameObject self;
+
+
+    void Reset()
+    {
+        self = this.gameObject;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this);
+    }
+
+    [ContextMenu("Set World Position")]
+    void SetWorldPosotion()
+    {
+        self.transform.position = target.transform.position;
+    }
+
+    [ContextMenu("Set World Rotation")]
+    void SetWorldRotation()
+    {
+        self.transform.rotation = target.transform.rotation;
+    }
+
+    [ContextMenu("Set World Scale")]
+    void SetWorldScale()
+    {
+        self.transform.localScale = target.transform.lossyScale;
+    }
+
+    [ContextMenu("Set Local Position")]
+    void SetLocalPosotion()
+    {
+        self.transform.position = target.transform.localPosition;
+    }
+
+    [ContextMenu("Set Local Rotation")]
+    void SetLocalRotation()
+    {
+        self.transform.rotation = target.transform.localRotation;
+    }
+
+    [ContextMenu("Set Local Scale")]
+    void SetLocalScale()
+    {
+        self.transform.localScale = target.transform.localScale;
+    }
+#endif
+
+}
