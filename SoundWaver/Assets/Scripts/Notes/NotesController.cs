b485a3f4c7372ed6a8e170ea86c0d5a3c91c2cb9
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-06 18:15:05 +0900

    note scripts add.

---
+++ SoundWaver/Assets/Scripts/Notes/NotesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesController : Yuuki.SingletonMonoBehaviour<NotesController>
+{
+    public List<INote> notes;
+
+    void Awake()
+    {
+        base.Awake();
+        notes = new List<INote>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        foreach (var it in notes)
+        {
+            it.Move();
+        }
+    }
+}
---
+++ SoundWaver/Assets/Scripts/Notes/SingleNote.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleNote : MonoBehaviour, INote
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Register();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Move()
+    {
+        Debug.Log("hoge");
+    }
+
+    public void Register()
+    {
+        NotesController.Instance.notes.Add(this);
+    }
+
+    public void Unregister()
+    {
+
+    }
+}
