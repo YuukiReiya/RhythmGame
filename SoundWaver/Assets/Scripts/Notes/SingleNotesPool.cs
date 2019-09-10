ac652608b181ababc374bb1653ebb2d88c36d2d6
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-10 13:55:01 +0900

    note pool scripts add.

---
+++ SoundWaver/Assets/Scripts/Notes/SingleNotesPool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class SingleNotesPool : SingletonObjectPool<SingleNotesPool>
+{
+    // Start is called before the first frame update
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+
+    [ContextMenu ("Create Pool Object Instances")]
+    protected override void CreatePoolObjects()
+    {
+        base.CreatePoolObjects();
+    }
+}
