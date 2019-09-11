d6643ac054e51f2c23cda74ac031eaa74d1943ba
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-11 09:29:15 +0900

    automatic music score reader script add.

---
+++ SoundWaver/Assets/Scripts/AutoMusicScore/AMSReader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class AMSReader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Read()
+    {
+        var file = new FileStream("specDatas.txt", FileMode.Open, FileAccess.Read);
+        var sr = new StreamReader(file);
+        List<float> buffer = new List<float>();
+        while (sr.EndOfStream != true)
+        {
+            string line = sr.ReadLine();
+            buffer.Add(float.Parse(string.IsNullOrEmpty(line) ? "0" : line));
+        }
+
+        //  リスト完成
+        int bpm = 158;
+        float inttime = 60 / bpm * 4;
+        for(int i=0;i<buffer.Count;++i)
+        {
+
+        }
+
+    }
+}
