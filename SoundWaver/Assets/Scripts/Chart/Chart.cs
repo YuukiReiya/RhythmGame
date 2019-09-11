6e79089cd9caf8b2fedcf8c21b92a626b53d8de8
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-11 12:08:50 +0900

    chart struct script add.

---
+++ SoundWaver/Assets/Scripts/Chart/Chart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Chart 
+{
+    public string Title;
+    public uint BPM;
+
+    //TODO:小節、拍子を入れる構造体に置換
+    //[System.Serializable]
+    //public struct Info
+    //{
+    //    public uint s;
+    //    public uint b;
+    //}
+    //public Info[] isb;
+
+
+    public float[] timing;
+}
