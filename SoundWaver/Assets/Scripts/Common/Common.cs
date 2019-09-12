c1db2dca44200872eede0fc65559dcf9332dbc38
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-12 14:44:10 +0900

    common scripts add.

---
+++ SoundWaver/Assets/Scripts/Common/Common.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common 
+{
+    public static class Define
+    {
+        /// <summary>
+        /// レーン数
+        /// </summary>
+        public const uint c_LaneCount = 3;
+
+        public const float c_PerfectTime = 0.033f;
+        public const float c_GreatTime = 0.1f;
+        public const float c_GoodTime = 0.2f;
+    }
+}
