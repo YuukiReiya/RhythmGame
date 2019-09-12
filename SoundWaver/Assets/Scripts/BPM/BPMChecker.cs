2a70df667f67bc020d0b21bd40e5b8ecc43a416b
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-12 14:42:16 +0900

    bpm check scripts add.

---
+++ SoundWaver/Assets/Scripts/BPM/BPMChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BPMChecker : MonoBehaviour
+{
+    [SerializeField] AudioClip checkAudioClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("Execute")]
+    public void Execute()
+    {
+        Debug.Log("BPM = " + UniBpmAnalyzer.AnalyzeBpm(checkAudioClip));
+    }
+}
