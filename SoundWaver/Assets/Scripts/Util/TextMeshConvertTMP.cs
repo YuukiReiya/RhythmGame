cdc7f0c7385a7df1c31c1ef11f1b09cc1db5dcfe
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-12 14:43:43 +0900

    TextMeshConvertTextMeshPro scipts add.
    MusicEngineのDebugTextが見づらかったので合わせて…
    その他の用途はなし。

---
+++ SoundWaver/Assets/Scripts/Util/TextMeshConvertTMP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class TextMeshConvertTMP : MonoBehaviour
+{
+    [SerializeField]
+    TextMesh textMesh;
+    [SerializeField]
+    TextMeshPro tmp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tmp.text = textMesh.text;
+    }
+}
