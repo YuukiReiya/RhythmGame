c74a0f982e7723a5d318ffc83538718cb868cda9
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-25 11:30:53 +0900

    file test scripts add.

---
+++ SoundWaver/Assets/Scripts/FileIO/Test1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    public UILabel label;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Execute();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Execute()
+    {
+        label.text = Application.persistentDataPath;
+    }
+}
