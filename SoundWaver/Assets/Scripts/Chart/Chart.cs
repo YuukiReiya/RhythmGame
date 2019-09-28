3c7dc4fa8ab210d853ebe372f42479a4871332bb
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-29 00:20:51 +0900

    update to any scripts.

--- SoundWaver/Assets/Scripts/Chart/Chart.cs
+++ SoundWaver/Assets/Scripts/Chart/Chart.cs
@@ -6,8 +6,14 @@ using UnityEngine;
 public struct Chart 
 {
     public string Title;
+    /// <summary>
+    /// 譜面データは一律同じ場所で連番で管理するようにする
+    /// </summary>
+    public string FilePath;//楽曲ファイルが格納されているパス
     public uint BPM;
-
+    public string ResistName;//登録名
+    public uint Number;//楽曲番号
+    public float NotesInterval;
     //TODO:小節、拍子を入れる構造体に置換
     //[System.Serializable]
     //public struct Info
--- SoundWaver/Assets/Scripts/Common/Common.cs
+++ SoundWaver/Assets/Scripts/Common/Common.cs
@@ -14,5 +14,19 @@ namespace Common
         public const float c_PerfectTime = 0.033f;
         public const float c_GreatTime = 0.1f;
         public const float c_GoodTime = 0.2f;
+
+        /// <summary>
+        /// ローカルファイル指定用のパス
+        /// </summary>
+        public const string c_LocalFilePath = "file:///";
+        
+        /// <summary>
+        /// MP3拡張子
+        /// </summary>
+        public const string c_MP3 = ".mp3";
+        /// <summary>
+        /// WAV拡張子
+        /// </summary>
+        public const string c_WAV = ".wav";
     }
 }
