d4a4d8f420e74fd65fe41626e97aad3a3d9333f3
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-28 23:24:22 +0900

    filemanager  scripts add.

---
+++ SoundWaver/Assets/Scripts/FileManager/FileManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yuuki.FileManager
+{
+    public class FileManager : SingletonMonoBehaviour<FileManager>
+    {
+        [System.Serializable]
+        struct FileTypePrefabs
+        {
+            public GameObject folder;
+            public GameObject file;
+        }
+        //serialize param
+        [SerializeField] FileTypePrefabs prefabs;
+        [SerializeField] UIGrid grid;
+        //private param
+        //public param
+        //accessor
+        public string CurrentDirectory { get; private set; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Test();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void UpdateCollider()
+        {
+            NGUITools.UpdateWidgetCollider(grid.gameObject);
+        }
+
+        GameObject Create(GameObject prefab, string labelName)
+        {
+            var inst = Instantiate(prefab);
+            inst.transform.parent = grid.transform;
+            inst.transform.localScale = prefab.transform.localScale;
+            FileUI ui;
+            if (!inst.TryGetComponent<FileUI>(out ui))
+            {
+                Destroy(inst);
+                return null;
+            }
+            ui.Setup(labelName);
+            return inst;
+        }
+
+        void Test()
+        {
+
+            foreach(var it in System.IO.Directory.GetDirectories(System.IO.Directory.GetCurrentDirectory()))
+            {
+                Debug.Log(it);
+                var name = System.IO.Path.GetFileName(it);
+                Debug.Log(name);
+                Create(prefabs.folder,name);
+            }
+            grid.Reposition();
+        }
+    }
+}
