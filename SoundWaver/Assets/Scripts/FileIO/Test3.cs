2add86f9e62d9336123f4735ebde2f3425f391d9
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-28 23:27:33 +0900

    test fileio scripts add.

---
+++ SoundWaver/Assets/Scripts/FileIO/Test3.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test3 : MonoBehaviour
+{
+    //public Yuuki.FileIO.ExternalFileIO exFileIO;
+    AudioSource source;
+    public string path;
+    AudioClip clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        Debug.Log("call");
+        path = ""+"a.mp3";
+        //exFileIO.GetAudioClip(path, ref clip,()=> { Execute(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Execute()
+    {
+        source.clip = clip;
+        source.Play();
+    }
+}
