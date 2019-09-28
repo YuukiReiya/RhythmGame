2d50fbb3f8b6c3f4ad069adb16d0391cfdfc8b27
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-29 00:15:54 +0900

    update lane script.

--- SoundWaver/Assets/Scripts/Lane/Lane.cs
+++ SoundWaver/Assets/Scripts/Lane/Lane.cs
@@ -4,72 +4,75 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using System.Linq;
 
-public class Lane : MonoBehaviour
+namespace Game
 {
-    [SerializeField,Tooltip("ノーツの流れるレーン番号")] uint laneNumber;
-    [SerializeField, Tooltip("関連付けさせるイベントトリガー")] EventTrigger eventTrigger;
-    private void Awake()
+    public class Lane : MonoBehaviour
     {
-        SetupTapEvent();
-    }
-
-    void SetupTapEvent()
-    {
-        EventTrigger.Entry entry = new EventTrigger.Entry();
-        entry.eventID = EventTriggerType.PointerDown;
-        entry.callback.AddListener((it) => { OnTapLane(); });
-        eventTrigger.triggers.Add(entry);
-    }
-
-    void OnTapLane()
-    {
-        Debug.Log("lane = " + laneNumber);
-
-        var notes = NotesController.Instance.notes;
-        if (notes.Where(it => it.LaneNumber == laneNumber).Count() == 0) { return; }
-        INote note = notes.
-            //対象のレーン
-            Where(it => it.LaneNumber == laneNumber).
-            //判定ラインに最も近いノーツ = (判定時間 - 再生時間 の差分が最も小さい)
-            OrderBy(it => Mathf.Abs(it.DownTime - NotesController.Instance.elapsedTime)).
-            First();
-
-        //ノーツが"n"秒経過していたら処理しない
-        //TODO:処理用確認
-        var tapTime = note.DownTime - NotesController.Instance.elapsedTime;
-        if (Mathf.Abs(tapTime) > NotesController.Instance.WaitTime) { return; }
-
-        //判定
-        string judge;
-
-        //TODO:汚い
-        if(Mathf.Abs(tapTime)<=Common.Define.c_PerfectTime)
+        [SerializeField, Tooltip("ノーツの流れるレーン番号")] uint laneNumber;
+        [SerializeField, Tooltip("関連付けさせるイベントトリガー")] EventTrigger eventTrigger;
+        private void Awake()
         {
-            judge = "perfect";
-            ScoreController.Instance.StartScoreEffect(ScoreController.Score.PERFECT);
+            SetupTapEvent();
         }
-        else if(Mathf.Abs(tapTime) <= Common.Define.c_GreatTime)
-        {
-            judge = "great";
-            ScoreController.Instance.StartScoreEffect(ScoreController.Score.GREAT);
-        }
-        else if (Mathf.Abs(tapTime) <= Common.Define.c_GoodTime)
-        {
-            judge = "good";
-            ScoreController.Instance.StartScoreEffect(ScoreController.Score.GOOD);
-        }
-        else
-        {
-            judge = "miss";
-            ScoreController.Instance.StartScoreEffect(ScoreController.Score.MISS);
-        }
-        Debug.Log(judge);
 
-        note.Unregister();
-    }
+        void SetupTapEvent()
+        {
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerDown;
+            entry.callback.AddListener((it) => { OnTapLane(); });
+            eventTrigger.triggers.Add(entry);
+        }
 
-    private void Reset()
-    {
-        if(!TryGetComponent<EventTrigger>(out eventTrigger)) { return; }
+        void OnTapLane()
+        {
+            Debug.Log("lane = " + laneNumber);
+
+            var notes = NotesController.Instance.notes;
+            if (notes.Where(it => it.LaneNumber == laneNumber).Count() == 0) { return; }
+            INote note = notes.
+                //対象のレーン
+                Where(it => it.LaneNumber == laneNumber).
+                //判定ラインに最も近いノーツ = (判定時間 - 再生時間 の差分が最も小さい)
+                OrderBy(it => Mathf.Abs(it.DownTime - NotesController.Instance.elapsedTime)).
+                First();
+
+            //ノーツが"n"秒経過していたら処理しない
+            //TODO:処理用確認
+            var tapTime = note.DownTime - NotesController.Instance.elapsedTime;
+            if (Mathf.Abs(tapTime) > NotesController.Instance.WaitTime) { return; }
+
+            //判定
+            string judge;
+
+            //TODO:汚い
+            if (Mathf.Abs(tapTime) <= Common.Define.c_PerfectTime)
+            {
+                judge = "perfect";
+                ScoreController.Instance.StartScoreEffect(ScoreController.Score.PERFECT);
+            }
+            else if (Mathf.Abs(tapTime) <= Common.Define.c_GreatTime)
+            {
+                judge = "great";
+                ScoreController.Instance.StartScoreEffect(ScoreController.Score.GREAT);
+            }
+            else if (Mathf.Abs(tapTime) <= Common.Define.c_GoodTime)
+            {
+                judge = "good";
+                ScoreController.Instance.StartScoreEffect(ScoreController.Score.GOOD);
+            }
+            else
+            {
+                judge = "miss";
+                ScoreController.Instance.StartScoreEffect(ScoreController.Score.MISS);
+            }
+            Debug.Log(judge);
+
+            note.Unregister();
+        }
+
+        private void Reset()
+        {
+            if (!TryGetComponent<EventTrigger>(out eventTrigger)) { return; }
+        }
     }
 }
