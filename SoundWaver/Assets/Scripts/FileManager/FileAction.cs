853c2fa03dda13f9e1fb8cad6ac77ffe61aaf4de
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-30 12:23:57 +0900

    update and change files.

---
+++ SoundWaver/Assets/Scripts/FileManager/FileAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yuuki.FileManager
+{
+    public class FileAction : MonoBehaviour
+    {
+        [SerializeField]public UILabel fileName;
+        public void SetupName(string fileName)
+        {
+            this.fileName.text = fileName;
+        }
+
+        public void SetupCurrentDirectory()
+        {
+            var currentDirectory = FileManager.Instance.CurrentDirectory + "\\" + fileName.text;
+            FileManager.Instance.UpdateCurrentDirectories(currentDirectory);
+            FileManager.Instance.Display();
+        }
+    }
+}
