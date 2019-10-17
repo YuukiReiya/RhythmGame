21395df98edad722ca71d976ac667e1d78cbd49c
Yuuki Bamba <yuukiBamba@gmail.com>
2019-10-17 13:15:34 +0900

    update framework script.

--- SoundWaver/Assets/Scripts/Framework/SingletonObjectPool.cs
+++ SoundWaver/Assets/Scripts/Framework/SingletonObjectPool.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
 public abstract class SingletonObjectPool<T> : Yuuki.SingletonMonoBehaviour<SingletonObjectPool<T>>
 {
-    [Header ("Parameter")]
+    //proteced param
+    [Header("Parameter")]
     [SerializeField] protected GameObject prefab;
     [SerializeField] protected uint poolCount;
     protected List<GameObject> poolList;
@@ -14,30 +13,37 @@ public abstract class SingletonObjectPool<T> : Yuuki.SingletonMonoBehaviour<Sing
     /// 
     /// オブジェクトを作った後にアクティブを切っているので、入れておきたい場合はオーバーライド
     /// </summary>
-    protected virtual void Setup ()
+    protected virtual void Setup()
     {
-        poolList = new List<GameObject> ();
+        poolList = new List<GameObject>();
         for (int i = 0; i < poolCount; ++i)
         {
-            poolList.Add (CreateNewInstance ());
+            poolList.Add(CreateNewInstance());
         }
         foreach (var it in poolList)
         {
-            it.SetActive (false);
+            it.SetActive(false);
         }
     }
 
-    protected virtual void GetAllChild<T> () where T : MonoBehaviour
+    /// <summary>
+    /// 子オブジェクトの全取得
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    protected virtual void GetAllChild<T>() where T : MonoBehaviour
     {
-        foreach (GameObject it in transform)
+        foreach (Transform it in transform)
         {
             T obj;
-            if (!it.TryGetComponent<T> (out obj)) { continue; }
-            poolList.Add (obj.gameObject);
+            if (!it.TryGetComponent<T>(out obj)) { continue; }
+            poolList.Add(obj.gameObject);
         }
     }
 
-    protected virtual void CreatePoolObjects ()
+    /// <summary>
+    /// プールするオブジェクトの作成
+    /// </summary>
+    protected virtual void CreatePoolObjects()
     {
         int childCount = transform.childCount;
         for (int i = 0; i < poolCount; ++i)
@@ -47,27 +53,35 @@ public abstract class SingletonObjectPool<T> : Yuuki.SingletonMonoBehaviour<Sing
         }
     }
 
-    public virtual GameObject GetObject ()
+    /// <summary>
+    /// オブジェクトの取得
+    /// </summary>
+    /// <returns></returns>
+    public virtual GameObject GetObject()
     {
         foreach (var it in poolList)
         {
             //  Activeなものは使用されている
             if (it.activeSelf) { continue; }
             //  Deactiveなものを見つけたらActiveにして返す。
-            it.SetActive (true);
+            it.SetActive(true);
             return it;
         }
         //  リストの中に使えるインスタンスがなかったため生成
-        var instance = CreateNewInstance ();
-        poolList.Add (instance);
+        var instance = CreateNewInstance();
+        poolList.Add(instance);
         return instance;
     }
 
-    private GameObject CreateNewInstance ()
+    /// <summary>
+    /// インスタンスの生成
+    /// </summary>
+    /// <returns></returns>
+    private GameObject CreateNewInstance()
     {
-        var instance = Instantiate (prefab);
+        var instance = Instantiate(prefab);
         instance.name = prefab.name + ":" + (poolList.Count + 1);
-        instance.transform.SetParent (this.transform);
+        instance.transform.SetParent(this.transform);
         return instance;
     }
 }
