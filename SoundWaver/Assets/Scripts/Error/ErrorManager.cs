e68445d5622d0632193f048803a37bc5912dae1f
Yuuki Bamba <yuukiBamba@gmail.com>
2019-10-17 13:54:14 +0900

    update error manager script.

--- SoundWaver/Assets/Scripts/Error/ErrorManager.cs
+++ SoundWaver/Assets/Scripts/Error/ErrorManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class ErrorManager : Yuuki.SingletonMonoBehaviour<ErrorManager>
 {
     public Text text;
+    public bool isActive = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +26,25 @@ public class ErrorManager : Yuuki.SingletonMonoBehaviour<ErrorManager>
     void Error()
     {
         Debug.LogError("error");
+    }
+
+    private void OnDestroy()
+    {
+        Save();
+    }
+
+    public void Save()
+    {
+#if UNITY_EDITOR
+        //エディタ上では空処理
+        if (!isActive) { return; }
+        Yuuki.FileIO.FileIO fileIO = new Yuuki.FileIO.FileIO();
+        var content = System.DateTime.Now.ToString() + "\n" + UnityEngine.SceneManagement.SceneManager.GetActiveScene().name + "\n\n" + text.text + "\n";
+        fileIO.CreateFile(Application.persistentDataPath + "/ロガー出力.txt", content, Yuuki.FileIO.FileIO.FileIODesc.Append);
+#elif UNITY_ANDROID
+        Yuuki.FileIO.FileIO fileIO = new Yuuki.FileIO.FileIO();
+        var content = System.DateTime.Now.ToString() + "\n" + UnityEngine.SceneManagement.SceneManager.GetActiveScene().name + "\n\n" + text.text + "\n";
+        fileIO.CreateFile(Application.persistentDataPath + "/ロガー出力.txt", content, Yuuki.FileIO.FileIO.FileIODesc.Append);
+#endif
     }
 }
