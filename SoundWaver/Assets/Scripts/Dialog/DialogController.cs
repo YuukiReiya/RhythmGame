5d87a9aa6ec10143e9d9452c7864caaf9437a24a
Yuuki Bamba <yuukiBamba@gmail.com>
2019-10-27 14:24:10 +0900

    add dialog script and prefab.

---
+++ SoundWaver/Assets/Scripts/Dialog/DialogController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogController : Yuuki.SingletonMonoBehaviour<DialogController>
+{
+    #region 構造体_宣言
+    [System.Serializable]
+    struct CheckDialog
+    {
+        public GameObject Parent;
+        public UILabel Message;
+        public System.Action Callback;
+
+        public void Open()
+        {
+            Parent.SetActive(true);
+        }
+        public void Close()
+        {
+            Callback?.Invoke();
+            Parent.SetActive(false);
+        }
+    }
+    #endregion
+    //serialize param
+    //[Header("")]
+    //[SerializeField] private UITexture fadeImage;
+    [Header("Check Dialog")]
+    [SerializeField] private CheckDialog checkDialog;
+    //private param
+    //pubic param
+  
+    public void Open(string message, System.Action callback = null)
+    {
+        checkDialog.Message.text = message;
+        checkDialog.Callback = callback;
+        checkDialog.Open();
+    }
+
+    public void Close()
+    {
+        checkDialog.Close();
+    }
+}
