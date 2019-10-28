4ec9d7248c020d796947c5b6e3b049185272269a
Yuuki Bamba <yuukiBamba@gmail.com>
2019-10-28 11:25:12 +0900

    update to fadecontroller.

--- SoundWaver/Assets/Scripts/Fade/FadeController.cs
+++ SoundWaver/Assets/Scripts/Fade/FadeController.cs
@@ -18,6 +18,7 @@ namespace API.Util
 
         //accessor
         public Queue<System.Action> EventQueue { get; private set; }
+        public IEnumerator Routine { get { return routine; } }
 
         protected override void Awake()
         {
@@ -29,30 +30,30 @@ namespace API.Util
                 Debug.LogError("Panelがアタッチされていない");
                 return;
             }
-            panel.alpha = 0.0f;
+            //panel.alpha = 0.0f;
         }
 
-        public void FadeIn(float time)
+        public void FadeIn(float time, float from = 0.0f, float to = 1.0f)
         {
             if (routine != null) { return; }
-            routine = Routine(0.0f, 1.0f, time);
-            this.StartCoroutine(routine,()=> { routine = null; });
-        }
-        public void FadeOut(float time)
-        {
-            if (routine != null) { return; }
-            routine = Routine(1.0f, 0.0f, time);
+            routine = Execute(from, to, time);
             this.StartCoroutine(routine, () => { routine = null; });
         }
-
-        private IEnumerator Routine(float from, float to, float time)
+        public void FadeOut(float time, float from = 1.0f, float to = 0.0f)
+        {
+            if (routine != null) { return; }
+            Debug.Log("yes");
+            routine = Execute(from, to, time);
+            this.StartCoroutine(routine, () => { routine = null; });
+        }
+        private IEnumerator Execute(float from, float to, float time)
         {
             float startTime = Time.time;
             panel.alpha = from;
-            while (Time.time < startTime + time)
+            while (Time.time <= startTime + time)
             {
                 float rate = time > 0.0f ? (Time.time - startTime) / time : 1.0f;
-                var alpha = Mathf.Lerp(0, 1, rate);
+                var alpha = Mathf.Lerp(from, to, rate);
                 panel.alpha = alpha;
                 yield return null;
             }
@@ -64,5 +65,24 @@ namespace API.Util
             EventQueue.Clear();
         }
 
+        public void Stop(bool callbackExecute=false)
+        {
+            if (routine != null) { 
+                StopCoroutine(routine);
+                routine = null;
+            }
+            if (callbackExecute)
+            {
+                foreach(var e in EventQueue)
+                {
+                    e?.Invoke();
+                }
+            }
+            EventQueue.Clear();
+        }
+        public void SetAlpha(float alpha)
+        {
+            panel.alpha = alpha;
+        }
     }
 }
