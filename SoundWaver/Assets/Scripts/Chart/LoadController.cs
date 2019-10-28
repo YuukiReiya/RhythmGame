37389e4ab848ad933be0deaa7b92ba4c9fc488ba
Yuuki Bamba <yuukiBamba@gmail.com>
2019-10-28 11:26:20 +0900

    update to loadcontroller.

--- SoundWaver/Assets/Scripts/Chart/LoadController.cs
+++ SoundWaver/Assets/Scripts/Chart/LoadController.cs
@@ -3,39 +3,76 @@ using UnityEngine;
 using Yuuki;
 using API.Util;
 using Common;
+using UnityEngine.SceneManagement;
 namespace Game
 {
 
     public class LoadController : SingletonMonoBehaviour<LoadController>
     {
-        IEnumerator routine;
+        //serialize param
+        private float startTime;
 
         // Start is called before the first frame update
         void Start()
         {
-            routine = null;
+            startTime = Time.time;
             FadeController.Instance.FadeOut(Define.c_FadeTime);
+            StartCoroutine(MainRoutine());
         }
 
-        // Update is called once per frame
-        void Update()
+        IEnumerator MainRoutine()
         {
-            if (routine == null)
+            //yield return new WaitForSeconds(1.0f);
+            //楽曲ファイルのロード中は待機
+            yield return new WaitWhile(
+                () => { return GameMusic.Instance.Clip.loadState != AudioDataLoadState.Loaded; }
+                );
+            //ロード状況によって切り替える
+            switch (GameMusic.Instance.Clip.loadState)
+            //switch(AudioDataLoadState.Failed)
             {
-                while (GameMusic.Instance.Clip.loadState != AudioDataLoadState.Loaded)
-                {
-                    return;
-                }
-                routine = Translation();
-                StartCoroutine(routine);
+                //読み込み成功
+                case AudioDataLoadState.Loaded: 
+                    OnLoadSuccess(); 
+                    break;
+                //読み込み失敗
+                case AudioDataLoadState.Failed:
+                case AudioDataLoadState.Unloaded:
+                    OnLoadFailed();
+                    break;
             }
-
         }
-
-        IEnumerator Translation()
+        /// <summary>
+        /// ロード成功処理
+        /// </summary>
+        private void OnLoadSuccess()
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
-            yield break;
+            var time = Time.time;
+            //フェード中にロードが完了してしまった場合
+            if ((time - startTime) < Define.c_FadeTime)
+            {
+                SceneManager.LoadScene("Game");
+            }
+            //フェード終了後にロード完了
+            else
+            {
+                FadeController.Instance.EventQueue.Enqueue(() => { UnityEngine.SceneManagement.SceneManager.LoadScene("Game"); });
+                FadeController.Instance.FadeIn(Define.c_FadeTime);
+            }
+        }
+        /// <summary>
+        /// ロード失敗処理
+        /// </summary>
+        private void OnLoadFailed()
+        {
+            FadeController.Instance.Stop();
+            DialogController.Instance.Open(
+                "楽曲が見つかりません。\nタイトルに戻ります",
+                () => {
+                             FadeController.Instance.EventQueue.Enqueue(() => { SceneManager.LoadScene("Start"); });
+                             FadeController.Instance.FadeIn(Define.c_FadeTime);
+                          }
+                );
         }
     }
 }
