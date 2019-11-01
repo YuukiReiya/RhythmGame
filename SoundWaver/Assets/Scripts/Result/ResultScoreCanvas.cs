32e7551467b0f77b43bace57e5ec511651ae3f30
Yuuki Bamba <yuukiBamba@gmail.com>
2019-11-01 09:31:24 +0900

    add result process scripts.

---
+++ SoundWaver/Assets/Scripts/Result/ResultScoreCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+public class ResultScoreCanvas : MonoBehaviour
+{
+    [System.Serializable]
+    struct ScoreLabels
+    {
+        public UILabel Perfect;
+        public UILabel Great;
+        public UILabel Good;
+        public UILabel Miss;
+    }
+    [SerializeField] private ScoreLabels labels;
+
+    public void Setup()
+    {
+        labels.Perfect.text = Judge.score.Perfect.ToString();
+        labels.Great.text = Judge.score.Great.ToString();
+        labels.Good.text = Judge.score.Good.ToString();
+        labels.Miss.text = Judge.score.Miss.ToString();
+    }
+}
---
+++ SoundWaver/Assets/Scripts/SceneController/ResultController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using API.Util;
+using Common;
+namespace Scenes
+{
+    public class ResultController : MonoBehaviour
+    {
+        //serialize param
+        [SerializeField] private ResultScoreCanvas resultScoreCanvas;
+        // Start is called before the first frame update
+        void Start()
+        {
+            resultScoreCanvas.Setup();
+            FadeController.Instance.FadeOut(Define.c_FadeTime);
+        }
+
+        public void TransitionSelect()
+        {
+            API.Util.FadeController.Instance.EventQueue.Enqueue(() => { UnityEngine.SceneManagement.SceneManager.LoadScene("Select"); });
+            API.Util.FadeController.Instance.FadeIn(Common.Define.c_FadeTime);
+        }
+    }
+}
