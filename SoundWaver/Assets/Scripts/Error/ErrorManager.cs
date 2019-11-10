498b0c8dd628aefe4c519c010a4269cbbecfb6cb
Yuuki Bamba <yuukiBamba@gmail.com>
2019-11-10 23:26:58 +0900

    update "ErrorManager.cs" script.
    MonoBehaviour⇒staticへ変更。
    DummyスクリプトでSetupの呼び出し。

--- SoundWaver/Assets/Scripts/Error/ErrorManager.cs
+++ SoundWaver/Assets/Scripts/Error/ErrorManager.cs
@@ -1,45 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-public class ErrorManager : Yuuki.SingletonMonoBehaviour<ErrorManager>
+using UnityEngine.SceneManagement;
+using Yuuki.FileIO;
+using Common;
+public static class ErrorManager
 {
-    public Text text;
-    public bool isActive = true;
-    // Start is called before the first frame update
-    void Start()
+    //serialize param
+    //private param
+    private static string contents;
+    //public param
+    //const param
+    const string c_LogFileName = "Log";
+    const string c_Extension = ".txt";
+
+    public static void Setup()
     {
         Application.logMessageReceived += HandleLog;
     }
 
-    void HandleLog(string condition, string stackTrace, LogType type)
+    private static void HandleLog(string condition, string stackTrace, LogType type)
     {
-        text.text += condition + "\n" + stackTrace + "\n" + type.ToString() + "\n";
+        contents+= condition + "\n" + stackTrace + "\n" + type.ToString() + "\n";
     }
 
-    [ContextMenu("Error")]
-    void Error()
+    public static void Save()
     {
-        Debug.LogError("error");
-    }
-
-    private void OnDestroy()
-    {
-        Save();
-    }
-
-    public void Save()
-    {
-#if UNITY_EDITOR
-        //エディタ上では空処理
-        if (!isActive) { return; }
-        Yuuki.FileIO.FileIO fileIO = new Yuuki.FileIO.FileIO();
-        var content = System.DateTime.Now.ToString() + "\n" + UnityEngine.SceneManagement.SceneManager.GetActiveScene().name + "\n\n" + text.text + "\n";
-        fileIO.CreateFile(Application.persistentDataPath + "/ロガー出力.txt", content, Yuuki.FileIO.FileIO.FileIODesc.Append);
-#elif UNITY_ANDROID
-        Yuuki.FileIO.FileIO fileIO = new Yuuki.FileIO.FileIO();
-        var content = System.DateTime.Now.ToString() + "\n" + UnityEngine.SceneManagement.SceneManager.GetActiveScene().name + "\n\n" + text.text + "\n";
-        fileIO.CreateFile(Application.persistentDataPath + "/ロガー出力.txt", content, Yuuki.FileIO.FileIO.FileIODesc.Append);
-#endif
+        FileIO fileIO = new FileIO();
+        var content = System.DateTime.Now.ToString() + "\n" + SceneManager.GetActiveScene().name + "\n\n" + contents + "\n";
+        fileIO.CreateFile(Application.persistentDataPath + Define.c_Delimiter + c_LogFileName + c_Extension, content, FileIO.FileIODesc.Append);
     }
 }
