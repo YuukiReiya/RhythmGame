639ab69135c7faee53adefcb15a90c12bb02f9dd
Yuuki Bamba <yuukiBamba@gmail.com>
2019-12-10 12:58:44 +0900

    add "score panel" script.
    ゲーム中のスコア表示用のスクリプトを追加。

---
+++ SoundWaver/Assets/Scripts/Score/ScorePanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// ゲーム中に表示されるスコア表示のUI
+    /// </summary>
+    public class ScorePanel : MonoBehaviour
+    {
+        [SerializeField] UILabel scoreLabel;
+
+        public void Setup()
+        {
+            StartCoroutine(MainRoutine());
+        }
+
+        private IEnumerator MainRoutine()
+        {
+            while (true)
+            {
+                scoreLabel.text = Judge.score.Point.ToString();
+                yield return null;
+            }
+        }
+    }
+}
