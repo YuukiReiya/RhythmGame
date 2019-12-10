1113369305ce5bd35b7583fcfc130db6819cdb22
Yuuki Bamba <yuukiBamba@gmail.com>
2019-12-10 13:03:21 +0900

    update "result controller" script.
    クリア状況の更新を行う関数を実装。
    ※要調整。

--- SoundWaver/Assets/Scripts/SceneController/ResultController.cs
+++ SoundWaver/Assets/Scripts/SceneController/ResultController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using API.Util;
 using Common;
+using Game;
+using Yuuki.FileIO;
 namespace Scenes
 {
     public class ResultController : MonoBehaviour
@@ -13,6 +15,7 @@ namespace Scenes
         void Start()
         {
             resultScoreCanvas.Setup();
+            SaveClearData();
             FadeController.Instance.FadeOut(Define.c_FadeTime);
         }
 
@@ -20,6 +23,36 @@ namespace Scenes
         {
             FadeController.Instance.EventQueue.Enqueue(() => { UnityEngine.SceneManagement.SceneManager.LoadScene("SelectDev"); });
             FadeController.Instance.FadeIn(Define.c_FadeTime);
+        }
+
+        private void SaveClearData()
+        {
+            var chart = ChartManager.Chart;
+
+            //TODO:RegistNameで判定すると、プリセットファイルの譜面が複製されてしまう。
+            //原因は FilePath != RegistName だから。 対策はChartに別途FilePathを加える
+            var path = Define.c_ChartSaveDirectory + Define.c_Delimiter + chart.ResistName + Define.c_JSON;
+
+            //パラメーターの更新
+            chart.wasCleared = true;//クリア済みフラグON
+
+            //ハイスコアが更新されれば保存
+            if (chart.Score < Judge.score.Point)
+            {
+                chart.Score = Judge.score.Point;
+                chart.ScoreCounts.Perfect = Judge.score.Perfect;
+                chart.ScoreCounts.Great = Judge.score.Great;
+                chart.ScoreCounts.Good = Judge.score.Good;
+                chart.ScoreCounts.Miss = Judge.score.Miss;
+                chart.Comb = Judge.score.MaxComb;
+            }
+
+            FileIO io = new FileIO();
+            io.CreateFile(
+                path,
+                JsonUtility.ToJson(chart),
+                FileIO.FileIODesc.Overwrite
+                );
         }
     }
 }
