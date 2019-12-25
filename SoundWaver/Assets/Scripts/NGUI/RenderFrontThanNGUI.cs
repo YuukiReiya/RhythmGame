28d41f5b5cba8b2776775cf018152cfaee9d1026
Yuuki Bamba <yuukiBamba@gmail.com>
2019-12-25 11:28:29 +0900

    add Util scripts files.
    マテリアルのレンダーキューを外部から編集可能にするUtilスクリプト と NGUIより手前にオブジェクトを描画するためのUtilスクリプト(レンダーキューをいじれば解決(使っていない?))の追加。

---
+++ SoundWaver/Assets/Scripts/NGUI/RenderFrontThanNGUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NGUI.Util
+{
+    /// <summary>
+    /// 3DオブジェクトをNGUIよりも手前で表示するためのUtilスクリプト
+    /// </summary>
+    public class RenderFrontThanNGUI : MonoBehaviour
+    {
+        [SerializeField, Tooltip("NGUIの描画はRenderQueue3000+Depthで計算されるので、それより大きい値を入力すること!")] uint renderQueue = 4000;
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
