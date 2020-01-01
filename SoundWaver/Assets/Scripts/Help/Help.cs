44ebe9a06c9b5976c175ca5ae0165d6b8b285d41
Yuuki Bamba <yuukiBamba@gmail.com>
2020-01-01 22:11:29 +0900

    add help script.
    ヘルプ管理のスクリプトの追加。

---
+++ SoundWaver/Assets/Scripts/Help/Help.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.UI;
+namespace ChartCreate
+{
+    public class Help : MonoBehaviour
+    {
+        //  serialize param
+        [SerializeField] private GameObject window;
+        [SerializeField] private RadioButton autoMatchBPM;
+        [SerializeField] private RadioButton bar;
+        [SerializeField] private RadioButton beat;
+        [SerializeField] private RadioButton interval;
+        [SerializeField] private RadioButton threshold;
+
+        private void Awake()
+        {
+            if (window.activeSelf)
+            {
+                window.SetActive(false);
+            }
+        }
+
+        public void Open()
+        {
+            window.SetActive(true);
+            autoMatchBPM.CallDisable();
+            bar.CallDisable();
+            beat.CallDisable();
+            interval.CallDisable();
+            threshold.CallDisable();
+        }
+
+        public void Close()
+        {
+            window.SetActive(false);
+        }
+    }
+}
