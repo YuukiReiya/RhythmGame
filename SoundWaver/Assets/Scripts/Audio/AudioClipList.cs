2029770787c9cb08182d81dda9a97323b9a928e7
Yuuki Bamba <yuukiBamba@gmail.com>
2020-01-02 22:02:08 +0900

    add Audio script.
    音関連のUtilスクリプトを追加。

---
+++ SoundWaver/Assets/Scripts/Audio/AudioClipList.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using API.Serialize;
+namespace API
+{
+    namespace Serialize
+    {
+        /// <summary>
+        /// 視覚化したいテーブルの基底クラス
+        /// </summary>
+        [System.Serializable]
+        public class TableBase<TKey, TValue, Type> where Type : KeyAndValue<TKey, TValue>
+        {
+            //private param
+            [SerializeField] List<Type> list;
+
+            //accessory
+            Dictionary<TKey, TValue> table { get; set; }
+
+            //property
+            public List<Type> List { get { return list; } }
+            public Dictionary<TKey, TValue> Table
+            {
+                get
+                {
+                    //null check
+                    if (table == null)
+                    {
+                        //convert List to Dictionary
+                        table = ConvertListToDictionary(list);
+                    }
+                    return table;
+                }
+            }
+
+            /// <summary>
+            /// ListからDictionaryへ変換
+            /// </summary>
+            /// <param name="list">変換するリスト</param>
+            /// <returns>変換したDictionary</returns>
+            static private Dictionary<TKey, TValue> ConvertListToDictionary(List<Type> list)
+            {
+                Dictionary<TKey, TValue> dic = new Dictionary<TKey, TValue>();
+                foreach (KeyAndValue<TKey, TValue> it in list) { dic.Add(it.key, it.value); }
+                return dic;
+            }
+
+        }
+
+        /// <summary>
+        /// シリアル化して視覚化するテーブルの要素(キーと値)
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        [System.Serializable]
+        public class KeyAndValue<TKey, TValue>
+        {
+            public TKey key;
+            public TValue value;
+
+            /// <summary>
+            /// 空コンストラクタ
+            /// </summary>
+            //        public KeyAndValue() { }
+
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            /// <param name="key">登録キー</param>
+            /// <param name="value">登録値</param>
+            public KeyAndValue(TKey key, TValue value)
+            {
+                this.key = key;
+                this.value = value;
+            }
+
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            /// <param name="pair">登録するペア</param>
+            public KeyAndValue(KeyAndValue<TKey, TValue> pair)
+            {
+                key = pair.key;
+                value = pair.value;
+            }
+        }
+    }
+
+}
+
+namespace Game.Audio
+{
+    [System.Serializable] public class AudioClipTable : TableBase<string, AudioClip, AudioPair> { };
+    [System.Serializable]
+    public class AudioPair : KeyAndValue<string, AudioClip>
+    {
+
+        public AudioPair(string key, AudioClip value) : base(key, value) { }
+    }
+
+    public class AudioClipList : MonoBehaviour
+    {
+        //  serialize param!
+        [SerializeField, Tooltip("AudioClipを格納するテーブル")] AudioClipTable table;
+
+        //  accessor
+        public AudioClipTable Table { get { return table; } }
+    }
+}
---
+++ SoundWaver/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Yuuki;
+using Common;
+using System;
+using Yuuki.MethodExpansions;
+
+namespace Game.Audio
+{
+    public class AudioManager : SingletonMonoBehaviour<AudioManager>
+    {
+        //serialize param
+        [SerializeField] private AudioSource sourceBGM;
+        [SerializeField] private AudioSource sourceSE;
+
+        //  private param
+        //  public param
+        [System.NonSerialized] public uint SEVolume;
+        [System.NonSerialized] public uint BGMVolume;
+        [System.NonSerialized] public AudioClipTable clips;
+        //accessor
+        public AudioSource SourceBGM { get { return sourceBGM; } }
+        public AudioSource SourceSE { get { return sourceSE; } }
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+         public float GetConvertVolume(uint vol)
+        {
+#if UNITY_EDITOR
+            if(vol<Define.c_MinVolume||Define.c_MaxVolume<vol)
+            {
+                Debug.LogError("AudioManager.cs line 33 GetConvertVolume value is invlid.");
+                return 0.0f;
+            }
+#endif
+            var A = 1.0f / Define.c_MaxVolume;
+            return (1.0f / Define.c_MaxVolume) * vol;
+        }
+
+        /// <summary>
+        /// BGMチャネルで再生
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="isLoop"></param>
+        public void PlayBGM(string key, bool isLoop = true)
+        {
+            if (!HasTableClipsKey(key))
+            {
+                Debug.LogError("AudioManager.cs line50 PlayBGM not found sound key.\n" + key);
+                ErrorManager.Save();
+                return;
+            }
+            sourceBGM.clip = clips.Table[key];
+            sourceBGM.loop = isLoop;
+            sourceBGM.Play();
+        }
+
+        /// <summary>
+        /// SEチャネルで再生
+        /// </summary>
+        public void PlaySE(string key, bool isLoop = false)
+        {
+            if (!HasTableClipsKey(key))
+            {
+                Debug.LogError("AudioManager.cs line66 PlaySE not found sound key.\n" + key);
+                ErrorManager.Save();
+                return;
+            }
+            sourceSE.clip = clips.Table[key];
+            sourceSE.loop = isLoop;
+            sourceSE.Play();
+        }
+
+        private bool HasTableClipsKey(string key)
+        {
+            return clips.Table.Keys.Contains(key);
+        }
+
+        public void Fade(AudioSource source, float time, float from, float to,  CoroutineExpansion.CoroutineFinishedFunc func = null)
+        {
+            this.StartCoroutine(FadeRoutine(source, time, from, to), func);
+        }
+
+        private IEnumerator FadeRoutine(AudioSource source, float time, float from, float to)
+        {
+            var st = Time.time;
+            while (Time.time <= st + time)
+            {
+                float rate = time > 0.0f ? (Time.time - st) / time : 1.0f;
+                var vol = Mathf.Lerp(from, to, rate);
+                source.volume = vol;
+                yield return null;
+            }
+            source.volume = to;
+        }
+    }
+}
