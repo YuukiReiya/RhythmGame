f7d8957a9edf34f1707d0593f991fdd049dc2988
Yuuki Bamba <yuukiBamba@gmail.com>
2020-01-07 13:00:05 +0900

    add Logger script.
    ゲームの中断/終了時にログをセーブするロガーを用意。
    テストしていない。

---
+++ SoundWaver/Assets/Scripts/Error/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yuuki;
+namespace API.Log
+{
+    /// <summary>
+    /// UnityAPIのコールバック処理の登録
+    /// </summary>
+    public class Logger : SingletonMonoBehaviour<Logger>
+    {
+        /// <summary>
+        /// ゲーム中断時にログを記録
+        /// </summary>
+        /// <param name="pause"></param>
+        private void OnApplicationPause(bool pause)
+        {
+            ErrorManager.Save();
+        }
+
+        /// <summary>
+        /// ゲーム終了時にログを記録
+        /// </summary>
+        private void OnApplicationQuit()
+        {
+            ErrorManager.Save();
+        }
+    }
+}
