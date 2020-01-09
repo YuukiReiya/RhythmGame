983c50fdd89c8c384198cb725468281041a8ebae
Yuuki Bamba <yuukiBamba@gmail.com>
2020-01-09 11:35:43 +0900

    add url open helper script.
    URL展開補助のスクリプト

---
+++ SoundWaver/Assets/Scripts/Util/OpenURLUtil.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// NGUIのUILabelを用いたハイパーリンク展開用補助スクリプト
+/// </summary>
+namespace Game.UI
+{
+    public class OpenURLUtil : MonoBehaviour
+    {
+        [SerializeField] UILabel label;
+        public void OpenLink()
+        {
+            var link = label.GetUrlAtPosition(UICamera.lastWorldPosition);
+            Application.OpenURL(link);
+        }
+
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            label = GetComponent<UILabel>();
+        }
+#endif
+    }
+}
