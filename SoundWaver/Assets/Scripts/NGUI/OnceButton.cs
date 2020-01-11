b48623fc58a45db0f7e2a463e04d0908d7e76069
Yuuki Bamba <yuukiBamba@gmail.com>
2020-01-11 17:13:15 +0900

    add press button color until script.
    ボタン押下後にUIを押下状態で表示するスクリプトの追加。
    ※導入は検討(非導入状態)。

---
+++ SoundWaver/Assets/Scripts/NGUI/OnceButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace API.Util
+{
+
+    public class OnceButton : MonoBehaviour
+    {
+        [SerializeField] private UIButton button;
+
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            button = GetComponent<UIButton>();
+        }
+#endif
+        public void Execute()
+        {
+            button.disabledColor = button.pressed;
+            button.isEnabled = false;
+        }
+    }
+
+}
